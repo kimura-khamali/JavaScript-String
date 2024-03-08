d2e4b628d2388702e3e825d56248b34a26535f19
kimura-khamali <kimurakhamali@gmail.com>
2024-03-08 09:12:51 +0300

    learning on operator

--- operator.js
+++ operator.js
@@ -12,25 +12,6 @@ const food1 = "over the lazy dog";
 console.log(`${food} eat  ${food1} `);
 //Count how many times the following string appears in the string variable:1. "the"2."brown"const story= "The quick brown fox jumps over the lazy dog"
 
-const story = "The quick brown fox jumps over the lazy dog";
-
-function countWord(story) {
-  const result = {};
-
-  const arr = story.split(" ");
-
-  for (let word of arr) {
-    if (result[word]) {
-      result[word]++;
-    } else {
-      result[word] = 1;
-    }
-  }
-
-  return result;
-}
-
-console.log(countWord("The brown the"));
 
 //Using JavaScript, find the following words from the following strings:1. "are"const string1 = "The pupils are reading in the library";2. "sitting"const string2 = "The child was sitting on the table before it fell"
 
@@ -49,18 +30,4 @@ console.log(name1.toUpperCase());
 console.log(name2.toLowerCase());
 console.log(name3.toLowerCase());
 
-function toTitleCase(str) {
-  if (!str) {
-    return "";
-  }
-
-  return str
-    .toLowerCase()
-    .split(" ")
-    .map(function (word) {
-      return word.charAt(0).toUpperCase().concat(word.substr(1));
-    })
-    .join(" ");
-}
-
-console.log(toTitleCase("A wonderful world"));
+console.log(name4.toUpperCase());
